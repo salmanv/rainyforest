8794a9c4bdfd7f7b84a6926477d2bb9634dcea86
Salman Vajid <s@vajid.com>
2014-05-28 14:08:00 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rainyforest::Application.config.secret_key_base = '290ca8e69718e1325231928b0363362b3e965a85b4828c98423938e25587f0f2648381b556bfc53faee8cd3778e1e1c5f2fba007883c44188b03f664948fc1fe'
